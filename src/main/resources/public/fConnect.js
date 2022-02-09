f68bef51bd21c935a3ad39c1e254143f6943e4cb
unknown <mateomejia401@gmail.com>
2022-02-08 22:50:35 -0500

    Despliegue en heroku

--- src/main/resources/public/fConnect.js
+++ src/main/resources/public/fConnect.js
@@ -6,6 +6,7 @@ var fConnect = (function(){
 
         connection : function(name, numero){
             var url = 'http://localhost:4567/convertir/' + name + '/' + numero
+            var heroku = "https://hidden-caverns-65840.herokuapp.com/"
                         fetch(url, {
                             mode: 'no-cors',
                             method: 'GET',
