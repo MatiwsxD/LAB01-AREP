5fcb2aacd43dd1827b5e5f30346a6def75e9d86b
unknown <mateomejia401@gmail.com>
2022-02-11 20:53:04 -0500

    Despliegue heroku

--- src/main/java/edu/escuelaing/arep/Convert.java
+++ src/main/java/edu/escuelaing/arep/Convert.java
@@ -7,12 +7,12 @@ public class Convert {
     public  String celToFa(Float cantidad){
         Float fahrenheit = cantidad*9/5 +32 ;
         String symbol = "°F";
-        return Float.toString(fahrenheit)+symbol;
+        return "El resultado es: " + Float.toString(fahrenheit)+symbol;
     }
     public  String faToCel(Float cantidad){
         float celcius = (cantidad*5 -160)/9;
         String symbol ="°C";
-        return Float.toString(celcius)+symbol;
+        return "El resultado es: " + Float.toString(celcius)+symbol;
     }
 
 
